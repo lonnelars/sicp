02e24b722fdfc000b62595bac6edf4e353220737
Lars Lønne <lonnelars@gmail.com>
2017-02-26 21:28:09 +0100

    feat: started ex 1.11

--- chapter1.2.scm
+++ chapter1.2.scm
@@ -49,4 +49,43 @@
 (define (g n) (A 1 n))                  ;(g n) is 2^n
 (define (h n) (A 2 n))                  ;(h n) is a "power tower"; 2^2^...^2, n times. See http://mathworld.wolfram.com/AckermannFunction.html
 
+;; 1.2.2 Tree recursion
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (fib-v2 n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;; exercise 1.11
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
 
