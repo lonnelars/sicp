17be36c7bef7a7e18f9853c92a9e3b9cd66232c3
Lars Loenne <lars.lonne@kantega.no>
2020-10-19 21:16:56 +0200

    exponentiation

--- section1.2.scm
+++ section1.2.scm
@@ -100,3 +100,31 @@
 	      b
 	      (- n 1))))
   (loop 2 1 0 n))
+
+(define (pascal row column)
+  (if (or (= column 1) (= column row))
+      1
+      (+ (pascal (- row 1) (- column 1))
+	 (pascal (- row 1) column))))
+
+;; 1.2.4 Exponentiation
+
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+		 (- counter 1)
+		 (* b product))))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+	((even? n) (square (fast-expt b (/ n 2))))
+	(else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
